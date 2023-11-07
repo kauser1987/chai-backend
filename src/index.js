a6dad64058e5be2b4e18abc3771f8bddd1bf4b34
kauser1987 <kauser1987@gmail.com>
2023-11-07 14:56:36 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,15 +1,68 @@
 // require('dotenv').config({path:'./env'});
+import express from "express"
 import dotenv from "dotenv"
+const app = express()
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
 dotenv.config({
     path:'./env'
 });
+connectDB()
+.then(()=>{
+    app.on("ERROR", (err)=>{
+                    console.log("ERROR:", err);
+                    throw err
+                })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`srver is running at port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MONGO DB connection failed !!!", error);
+})
 
 
 
-connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // import express from "express";
 // const app=express()
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,25 @@
+ const asynchandler =(fn)=>{ (req, res, next)=>{ 
+    Promise.resolve(fn(req, res, next)).catch((err)=>next(err))
+    }
+
+ }
+
+
+
+
+
+
+// const asynchandler = (fn) => async(req, res, next) => {
+    
+//     try {
+//         await fn(req, res, next)
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })        
+//     }
+// }
+
+
+export default asynchandler;
