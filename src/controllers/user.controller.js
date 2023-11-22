826e562a762852ba716f266674c1bab0e885fe31
kauser1987 <kauser1987@gmail.com>
2023-11-22 12:03:00 +0530

    minor update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
-import asynchandler from "../utils/asynchandler";
+import {asynchandler} from "../utils/asynchandler.js";
 
 const registerUser = asynchandler(async (req, res)=>{
  return res.status(200).json({
-        message:"ok"
+        message:"ok",
     })
 }) 
 
